88e94ff122e377f48df85b18377a7fbe303e22bb
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-11 20:37:32 +0200

    Adding LoginRequiredMixin to the HomeView.

--- web/home/views.py
+++ web/home/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
 
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
