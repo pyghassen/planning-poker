68888ad14a887306607f011c73395e4f7a1a2e7e
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-11 23:40:25 +0200

    Adding login step for the tests for the HomeView.

--- web/home/tests.py
+++ web/home/tests.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from allauth.utils import get_user_model
+
+
 class HomeViewTest(TestCase):
+    def setUp(self):
+        username = 'testuser'
+        password = 'testpass'
+        User = get_user_model()
+        user = User.objects.create_user(username, password=password)
+        logged_in = self.client.login(username=username, password=password)
+
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
