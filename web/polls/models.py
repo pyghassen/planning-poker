3b2f3dfc98b4d3fb445a895b59b1b66713ebed18
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-12 01:29:47 +0200

    Adding the Task model class.

--- web/polls/models.py
+++ web/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
