e8e0ecde0ba50a213b6299e02cd22935329ac4e5
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-12 12:03:24 +0200

    Adding the websocket broadcast message when vote is created.

--- web/polls/forms.py
+++ web/polls/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 from polls.models import Task, Vote
 
@@ -35,4 +37,9 @@ class VoteForm(forms.ModelForm):
             task_id=self.task_id,
             defaults={'value': self.instance.value}
         )
+        layer = get_channel_layer()
+        async_to_sync(layer.group_send)('events', {
+            'type': 'events.alarm',
+            'content': 'triggered'
+        })
         return vote
