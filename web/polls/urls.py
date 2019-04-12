439db75c3b5edf0f8caea03ad7d986939ac45e73
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-12 10:08:55 +0200

    Adding the VoteCreateView to the polls urls.

--- web/polls/urls.py
+++ web/polls/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from polls.views import TaskCreateView, TaskListView, TaskDetailView
+from polls.views import (
+    TaskCreateView, TaskListView, TaskDetailView, VoteCreateView
+)
+
 
 urlpatterns = [
     path('task/create', TaskCreateView.as_view(), name='task-create'),
     path('task/list', TaskListView.as_view(), name='task-list'),
     path('task/<int:pk>:', TaskDetailView.as_view(), name='task-detail'),
+    path('vote/create/<int:task_id>:', VoteCreateView.as_view(), name='vote-create'),
 ]
