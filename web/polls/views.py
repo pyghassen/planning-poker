83e421f8af3f0839a189c767145805bc019c8993
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-12 02:27:09 +0200

    Adding the TaskCreateView and TaskListView classes to the polls views.

--- web/polls/views.py
+++ web/polls/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from polls.models import Task
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ['name']
+    success_url = reverse_lazy('task-list')
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
