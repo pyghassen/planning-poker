0edca2aff76cc94c4a7b6ca2968d5046f92fdad9
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-12 08:36:50 +0200

    Adding the TaskDetailView to the urls.

--- web/polls/urls.py
+++ web/polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from polls.views import TaskCreateView, TaskListView
+from polls.views import TaskCreateView, TaskListView, TaskDetailView
 
 urlpatterns = [
     path('task/create', TaskCreateView.as_view(), name='task-create'),
     path('task/list', TaskListView.as_view(), name='task-list'),
+    path('task/<int:pk>:', TaskDetailView.as_view(), name='task-detail'),
 ]
