29a234196d5694d2148a29e592abd10e2e402c06
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-12 11:59:43 +0200

    Adding value for the CACHE_HOST settings.

--- web/config/wsgi.py
+++ web/config/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('CACHE_HOST', '127.0.0.1')
 
 application = get_wsgi_application()
--- web/manage.py
+++ web/manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault('CACHE_HOST', '127.0.0.1')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
