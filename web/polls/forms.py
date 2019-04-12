f7576b663d154948b8cd4bd60793eeb090df937c
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-12 12:09:05 +0200

    Commenting the breaking code.

--- web/polls/forms.py
+++ web/polls/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
+# from channels.layers import get_channel_layer
+# from asgiref.sync import async_to_sync
 
 from polls.models import Task, Vote
 
@@ -37,9 +37,9 @@ class VoteForm(forms.ModelForm):
             task_id=self.task_id,
             defaults={'value': self.instance.value}
         )
-        layer = get_channel_layer()
-        async_to_sync(layer.group_send)('events', {
-            'type': 'events.alarm',
-            'content': 'triggered'
-        })
+        # layer = get_channel_layer()
+        # async_to_sync(layer.group_send)('events', {
+        #     'type': 'events.alarm',
+        #     'content': 'triggered'
+        # })
         return vote
