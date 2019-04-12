f2a11f82e08aea74578ebc2d093f4882bde2f5a8
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-12 02:31:01 +0200

    Customizing the TaskCreateView to take the user from session.

--- web/polls/views.py
+++ web/polls/views.py
@@ -3,12 +3,22 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 from polls.models import Task
+from pools.forms import TaskForm
 
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
-    fields = ['name']
     success_url = reverse_lazy('task-list')
+    form_class = TaskForm
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update(
+            {
+                'user': self.request.user,
+            }
+        )
+        return kwargs
 
 
 class TaskListView(LoginRequiredMixin, ListView):
