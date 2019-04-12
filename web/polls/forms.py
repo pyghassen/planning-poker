da83b966a66d6d80d6b46e026dee7c23e6bd3ae1
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-12 10:08:21 +0200

    Adding customized VoteForm to take the User from the session and the task_id from the url.

--- web/polls/forms.py
+++ web/polls/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from polls.models import Task
+from polls.models import Task, Vote
 
 class TaskForm(forms.ModelForm):
 
@@ -16,3 +16,23 @@ class TaskForm(forms.ModelForm):
     def save(self, *args, **kwargs):
         self.instance.created_by=self.user
         return super().save(*args, **kwargs)
+
+
+class VoteForm(forms.ModelForm):
+
+    class Meta:
+        model = Vote
+        fields = ['value']
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        self.task_id = kwargs.pop('task_id')
+        return super().__init__(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        vote, _ = Vote.objects.update_or_create(
+            user=self.user,
+            task_id=self.task_id,
+            defaults={'value': self.instance.value}
+        )
+        return vote
