ea60d588ba40a2df8fb5ce026ffa80d1f01a9e25
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-28 11:59:36 +0200

    Linting the home viewfile.

--- web/home/views.py
+++ web/home/views.py
@@ -1,6 +1,13 @@
+"""
+The home view module.
+"""
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
+    """
+    HomeView class definition, it uses the LoginRequiredMixin to require
+    authentication to access the view, and it returns a TemplateView
+    """
     template_name = 'home.html'
