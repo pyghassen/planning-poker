b1f03b0043b6067369e2cc89ff70d9dad834f685
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-28 11:59:28 +0200

    Linting the home view test file.

--- web/home/tests.py
+++ web/home/tests.py
@@ -1,3 +1,6 @@
+"""
+Test home view module.
+"""
 from django.test import TestCase
 from django.urls import reverse
 
@@ -5,22 +8,35 @@ from allauth.utils import get_user_model
 
 
 class HomeViewTest(TestCase):
+    """HomeView test class."""
     def setUp(self):
         username = 'testuser'
         password = 'testpass'
-        User = get_user_model()
-        user = User.objects.create_user(username, password=password)
+        user_model_class = get_user_model()
+        user_model_class.objects.create_user(username, password=password)
         self.client.login(username=username, password=password)
 
     def test_view_url_exists_at_desired_location(self):
+        """
+        Verifying getting 200 as status code when we send request to `/`.
+        """
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
+        """
+        Verifying getting 200 as status code when we send request to `/` while
+        we use reverse function to get the URL.
+        """
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
+        """
+        Verifying getting 200 as status code when we send request to `/` while
+        we use reverse function to get the URL and make sure we use correct
+        tempalte for the response.
+        """
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
