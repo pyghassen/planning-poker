ffbfa5715d7c3adb2dbb71e81487bd72b159068f
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-28 17:24:23 +0200

    Adding docstring to the forms file.

--- web/polls/forms.py
+++ web/polls/forms.py
@@ -1,37 +1,60 @@
+"""
+Forms module contains the TaskForm and the VoteForm classes definition.
+"""
 from django import forms
 
 
 from polls.models import Task, Vote
 
 class TaskForm(forms.ModelForm):
-
-    class Meta:
+    """Task form class definition."""
+    class Meta: # pylint: disable=R0903,C0111
         model = Task
         fields = ['name']
 
 
     def __init__(self, *args, **kwargs):
+        """
+        Gets the `user` instance from the kwargs and assigns it it the `user`
+        class attribue.
+        """
         self.user = kwargs.pop('user')
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
-    def save(self, *args, **kwargs):
-        self.instance.created_by=self.user
-        return super().save(*args, **kwargs)
+    def save(self, commit=True):
+        """
+        Gets the `user` class attribute value to assign it to the `created_by`
+        `Task` model field when saving to the database.
+        """
+        self.instance.created_by = self.user
+        return super().save()
 
 
 class VoteForm(forms.ModelForm):
-
-    class Meta:
+    """Vote form class definition."""
+    class Meta: # pylint: disable=R0903,C0111
         model = Vote
         fields = ['value']
 
     def __init__(self, *args, **kwargs):
+        """
+        Gets the `user` instance and `task_id` from the kwargs and assigns them
+        as class attribue.
+        """
         self.user = kwargs.pop('user')
         self.task_id = kwargs.pop('task_id')
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
-    def save(self, *args, **kwargs):
-        vote, _ = Vote.objects.update_or_create(
+    def save(self, commit=True):
+        """
+        Gets the `user` and the `task_id` class attribute value to assign it to
+        the `user`, `task_id` in the `Vote` model fields when saving to the
+        database.
+
+        The `update_or_create` method was used to prevent creating object every
+        time, which is the default behaviour, but update the Vote if exists.
+        """
+        vote, _ = Vote.objects.update_or_create( # pylint: disable=E1101
             user=self.user,
             task_id=self.task_id,
             defaults={'value': self.instance.value}
