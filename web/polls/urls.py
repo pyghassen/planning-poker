68a59a5729cd669c50d4a70e5fbe296634637a43
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-28 15:07:40 +0200

    Linting polls urls file.

--- web/polls/urls.py
+++ web/polls/urls.py
@@ -1,10 +1,16 @@
+"""
+Poll app URL Configuration
+
+The `urlpatterns` list routes URLs to views which will be included in the main
+urls file.
+"""
 from django.urls import path
 from polls.views import (
     TaskCreateView, TaskListView, TaskDetailView, VoteCreateView
 )
 
 
-urlpatterns = [
+urlpatterns = [ # pylint: disable=C0103
     path('task/create', TaskCreateView.as_view(), name='task-create'),
     path('task/list', TaskListView.as_view(), name='task-list'),
     path('task/<int:pk>:', TaskDetailView.as_view(), name='task-detail'),
