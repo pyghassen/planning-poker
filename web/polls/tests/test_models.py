6eab93665f79a59503810510e5bce4947a8e6816
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-28 12:10:07 +0200

    Adding docstrings to the model test file.

--- web/polls/tests/test_models.py
+++ web/polls/tests/test_models.py
@@ -1,10 +1,14 @@
+"""
+Task model test module.
+"""
 from django.test import TestCase
 
 from polls.models import Task
 
 
 class TaskModelTest(TestCase):
-
+    """TaskModel test calss definition."""
     def test_string_representation(self):
+        """Verifies calling the __str__ method returns the expected string."""
         task = Task(name="Task 1")
         self.assertEqual(str(task), task.name)
