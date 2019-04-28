2329526501b7da890a300a4ef37e868a3f52b7c1
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-28 17:29:29 +0200

    Addong docstring to the polls models file.

--- web/polls/models.py
+++ web/polls/models.py
@@ -1,3 +1,8 @@
+"""
+Poll model module that contains the Task model which has relation Vote model.
+One task has many votes, and every vote has one user.
+And user can be the creator or a task too.
+"""
 from django.db import models
 
 from django.contrib.auth.models import User
@@ -15,6 +20,7 @@ PLANNING_CARDS = (
 )
 
 class Task(models.Model):
+    """Task model definition."""
     name = models.CharField(max_length=200)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -25,6 +31,7 @@ class Task(models.Model):
 
 
 class Vote(models.Model):
+    """Vote model definition."""
     value = models.CharField(
         max_length=3,
         choices=PLANNING_CARDS,
