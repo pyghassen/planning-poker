0e56f215b7d482d2ce7d19999b452c26366e3f71
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2020-08-23 23:29:49 +0200

    fix(polls): Remove the extra column character in the URLs

--- web/polls/urls.py
+++ web/polls/urls.py
@@ -15,7 +15,7 @@ from polls.views import (
 urlpatterns = [ # pylint: disable=C0103
     path('task/create', TaskCreateView.as_view(), name='task-create'),
     path('task/list', TaskListView.as_view(), name='task-list'),
-    path('task/<int:pk>:', TaskDetailView.as_view(), name='task-detail'),
-    path('task/delete/<int:pk>:', TaskDeleteView.as_view(), name='task-delete'),
-    path('vote/create/<int:task_id>:', VoteCreateView.as_view(), name='vote-create'),
+    path('task/<int:pk>', TaskDetailView.as_view(), name='task-detail'),
+    path('task/delete/<int:pk>', TaskDeleteView.as_view(), name='task-delete'),
+    path('vote/create/<int:task_id>', VoteCreateView.as_view(), name='vote-create'),
 ]
