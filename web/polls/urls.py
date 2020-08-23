6281f50489baf46f32ab5be996f6e16b3a698b05
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2020-08-23 20:50:26 +0200

    feat(polls): Add delete task button in Task list view

--- web/polls/urls.py
+++ web/polls/urls.py
@@ -5,8 +5,10 @@ The `urlpatterns` list routes URLs to views which will be included in the main
 urls file.
 """
 from django.urls import path
+
 from polls.views import (
-    TaskCreateView, TaskListView, TaskDetailView, VoteCreateView
+    TaskCreateView, TaskListView, TaskDetailView, VoteCreateView,
+    TaskDeleteView
 )
 
 
@@ -14,5 +16,6 @@ urlpatterns = [ # pylint: disable=C0103
     path('task/create', TaskCreateView.as_view(), name='task-create'),
     path('task/list', TaskListView.as_view(), name='task-list'),
     path('task/<int:pk>:', TaskDetailView.as_view(), name='task-detail'),
+    path('task/delete/<int:pk>:', TaskDeleteView.as_view(), name='task-delete'),
     path('vote/create/<int:task_id>:', VoteCreateView.as_view(), name='vote-create'),
 ]
